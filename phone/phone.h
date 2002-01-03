faf1ff4f26a82dfe6087b9d305ff57d34b45990c
David Sugar <dyfet@gnutelephony.org>
2002-01-03 13:15:02 +0000

    header fixup

--- phone/phone.h
+++ phone/phone.h
@@ -14,10 +14,10 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  
-#ifndef __CCXX_PHONE_H__
-#define __CCXX_PHONE_H__
+#ifndef CCXX_PHONE_H_
+#define CCXX_PHONE_H_
  
-#ifndef __CCXX_RTP_H__
+#ifndef CCXX_RTP_H_
 #include <cc++/rtp.h>
 #endif
  
