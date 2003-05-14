2f042ea472edf8f025c34cc3bde0dde884d43446
Federico Montesino Pouzols <fedemp@altern.org>
2003-05-14 11:58:51 +0000

    updates

--- src/ccrtp/sources.h
+++ src/ccrtp/sources.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2001,2002 Federico Montesino Pouzols <fedemp@altern.org>.
+// Copyright (C) 2001,2002,2003 Federico Montesino Pouzols <fedemp@altern.org>.
 // 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -257,7 +257,7 @@ public:
 	/**
 	 * Whether this source sends RTP data packets.
 	 **/
-	bool isSender()
+	bool isSender() const
 	{ return activeSender; }
 
 	uint32 getID() const
